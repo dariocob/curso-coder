6c5aeb578ef69f7feb7b0e0d1fd4fee06364a61c
dariocob <74957011+dariocob@users.noreply.github.com>
2022-04-06 21:34:26 -0300

    Add files via upload

---
+++ eventos.js
@@ -0,0 +1,6 @@
+<script>
+    function getProd() {
+    const f = document.getElementById("SelProd").value;
+    document.getElementById("pf").innerHTML = "El producto seleccionada es: " + f;
+}
+</script>
